87c10b8ecb99b0a4ad985b64f894ce78cacf9ea6
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-24 15:58:31 +0700

    fix user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+        for ($i = 0; $i < 20; $i++) {
+            $fakerName = $faker->userName;
+            $type = "editor";
+            if ($i % 2) {
+                $type = "reporter";
+            }
+            DB::table('users')->insert([
+                'name'       => $fakerName,
+                'email'      => $fakerName . "@gmail.com",
+                'password'   => Hash::make('password'),
+                'type'       => $type,
+                'created_at' => Carbon::now()
+            ]);
+        }
+    }
+}
