ce9c047dacac5cead8271ddb0a32ea157e224e39
Putra Pratama Nasution <putrapratamans@gmail.com>
2021-02-24 20:39:39 +0700

    view ui login register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel gaws
                 </div>
 
                 <div class="links">
